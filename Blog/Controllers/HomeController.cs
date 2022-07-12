5ac3a2121cbdc002897ec18fbce3c72b6cfdf617
Reg1str <70968239+Reg1str@users.noreply.github.com>
2022-07-12 22:48:44 +0300

    Adding views, models and tag helper

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
-
 public class HomeController : Controller
 {
     public IActionResult Index()
     {
         return View();
     }
+    
+    public IActionResult Post()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Edit()
+    {
+        return View(new Post());
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Post post)
+    {
+        
+        return RedirectToAction("Index");
+    }
+
 }
