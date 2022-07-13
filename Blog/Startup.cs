e59b1345fa27942b375ce4e6dc7b7256c1f21636
Reg1str <70968239+Reg1str@users.noreply.github.com>
2022-07-13 23:22:09 +0300

    Database creating

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -1,9 +1,19 @@
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Blog
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));   
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
