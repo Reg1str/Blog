d12e414f53864d5b00466a81de42d12977b77652
Reg1str <70968239+Reg1str@users.noreply.github.com>
2022-07-13 23:27:31 +0300

    Dependency injection Adding posts to database

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
-﻿using Blog.Models;
+﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
 public class HomeController : Controller
 {
+    private AppDbContext _ctx;
+
+    public HomeController(AppDbContext ctx)
+    {
+        _ctx = ctx;
+    }
     public IActionResult Index()
     {
         return View();
@@ -21,8 +28,10 @@ public class HomeController : Controller
     }
     
     [HttpPost]
-    public IActionResult Edit(Post post)
+    public async Task<IActionResult> Edit(Post post)
     {
+        _ctx.Posts.Add(post);
+        await _ctx.SaveChangesAsync();
         
         return RedirectToAction("Index");
     }
