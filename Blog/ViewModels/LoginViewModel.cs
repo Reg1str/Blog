55b8887bdad697551dad40e3c63f819a0ec81f85
Reg1str <70968239+Reg1str@users.noreply.github.com>
2022-07-17 14:43:22 +0300

    Simple styles

--- Blog/ViewModels/LoginViewModel.cs
+++ Blog/ViewModels/LoginViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Blog.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels;
 
 public class LoginViewModel
 {
     public string UserName { get; set; }
+    
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
