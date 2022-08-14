35263e4052e6498c0ec8919386e1896518503696
Reg1str <ilya.n1konov@inbox.ru>
2022-08-14 15:21:55 +0300

    Publishing to WWW part2

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -14,22 +14,36 @@ public class HomeController : Controller
         _fileManager = fileManager;
     }
     
-    public IActionResult Index(string category)
-    {
-        var posts = string.IsNullOrEmpty(category) ? _repository.GetAllPosts() : _repository.GetAllPosts(category);
-        return View(posts);
-    }
-    
-    public IActionResult Post(int id)
-    {
-        var post = _repository.GetPost(id);
-        return View(post);
-    }
+    public IActionResult Index(string category) => 
+        View(string.IsNullOrEmpty(category) ? 
+            _repository.GetAllPosts() : 
+            _repository.GetAllPosts(category));
 
+    public IActionResult Post(int id) => 
+            View(_repository.GetPost(id));
+    
     [HttpGet("/Image/{image}")]
-    public IActionResult Image(string image)
-    {
-        var mime = image.Substring(image.LastIndexOf('.') + 1);
-        return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
-    }
+        public IActionResult Image(string image) => 
+            new FileStreamResult(
+                _fileManager.ImageStream(image), 
+                $"image/{image.Substring(image.LastIndexOf('.') + 1)}");
+    
+    // public IActionResult Index(string category)
+    // {
+    //     var posts = string.IsNullOrEmpty(category) ? _repository.GetAllPosts() : _repository.GetAllPosts(category);
+    //     return View(posts);
+    // }
+
+    // public IActionResult Post(int id)
+    // {
+    //     var post = _repository.GetPost(id);
+    //     return View(post);
+    // }
+
+    // [HttpGet("/Image/{image}")]
+    // public IActionResult Image(string image)
+    // {
+    //     var mime = image.Substring(image.LastIndexOf('.') + 1);
+    //     return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
+    // }
 }
