ff93aaac6bd8427d7b373703389c50a672f1a05e
Reg1str <ilya.n1konov@inbox.ru>
2022-08-15 22:02:16 +0300

    Pagination fix & optimization

--- Blog/Data/Repository/Repository.cs
+++ Blog/Data/Repository/Repository.cs
@@ -1,8 +1,8 @@
-﻿using Blog.Models;
+﻿using Blog.Helpers;
+using Blog.Models;
 using Blog.Models.Comments;
 using Blog.ViewModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Blog.Data.Repository;
 
@@ -47,7 +47,7 @@ public class Repository : IRepository
             PageNumber = pageNumber,
             PageCount = pageCount,
             NextPage = postsCount > skipAmount + pageSize,
-            Pages = PageNumbers(pageNumber, pageCount),
+            Pages = PageHelper.PageNumbers(pageNumber, pageCount).ToList(),
             Category = category,
             Posts = query
                 .Skip(skipAmount)
@@ -55,39 +55,6 @@ public class Repository : IRepository
                 .ToList()
 
         };
-    }
-
-    private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
-    {
-        int midPoint = pageNumber < 3 ? 3
-            : pageNumber > pageCount - 2 ? pageCount - 2
-            : pageNumber;
-
-        int lowerBound = midPoint - 2;
-        int upperBound = midPoint + 2;
-        
-        if (lowerBound != 1)
-        {
-            yield return 1;
-            if (lowerBound - 1 > 1)
-            {
-                yield return -1;
-            }
-        }
-        
-        for (int i = midPoint -2; i <= midPoint + 2; i++)
-        {
-            yield return i;
-        }
-
-        if (upperBound != pageCount)
-        {
-            if (pageCount - upperBound > 1)
-            {
-                yield return -1;
-            }
-            yield return pageCount;
-        }
     }
 
     public void AddPost(Post? post)
