e28f824af45bf6338e4cd300e46e5959ada889ea
Reg1str <ilya.n1konov@inbox.ru>
2022-08-15 23:08:42 +0300

    Sign Up for users

--- Blog/Controllers/AuthController.cs
+++ Blog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.ViewModels;
+﻿using System.Net;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,13 @@ namespace Blog.Controllers;
 public class AuthController : Controller
 {
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public AuthController(SignInManager<IdentityUser> signInManager)
+    public AuthController(SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     [HttpGet]
@@ -23,8 +27,49 @@ public class AuthController : Controller
     public async Task<IActionResult> Login(LoginViewModel viewModel)
     {
         var result = await _signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, false, false);
+        if (!result.Succeeded)
+        {
+            return View(viewModel);
+        }
+
+        var user = await _userManager.FindByNameAsync(viewModel.UserName);
+        var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+        if (isAdmin)
+        { 
+            return RedirectToAction("Index", "Panel");  
+        }
         
-        return RedirectToAction("Index", "Panel");
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View(new RegisterViewModel());
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+        
+        var user = new IdentityUser
+        {
+            UserName = viewModel.Email,
+            Email = viewModel.Email
+        };
+        var result = await _userManager.CreateAsync(user, "password");
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View();
     }
 
     public async Task<IActionResult> Logout()
